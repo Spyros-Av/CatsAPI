81e1e834a4e27f0d2187b64389e1d7c488322115
Spyros-Av <77226415+Spyros-Av@users.noreply.github.com>
2026-03-10 10:28:16 +0200

    Initial project setup with core infrastructure
    - Created folder structure
    -Added required NuGet packages:
      * Microsoft.EntityFrameworkCore
      * Microsoft.EntityFrameworkCore.SqlServer
    - Added entity classes
    - Implemented DbContext

---
+++ CatsAPI/CatsAPI/Models/Entities/TagEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace CatsAPI.Models.Entities
+{
+    public class TagEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public List<CatEntity> Cats { get; set; } = new List<CatEntity>();
+    }
+}
