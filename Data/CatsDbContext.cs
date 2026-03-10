74c05cf392ad24d9bbcc4c69b7dae82d8311db01
Spyros-Av <77226415+Spyros-Av@users.noreply.github.com>
2026-03-10 13:35:23 +0200

    Created DTOs and configure CatsDbContext
    - CatsDbContext with CatEntity and TagEntity
    - Registered CatsDbContext  in program.cs
    - Added CatsConnectionString to appsetting.json
    - Created CatDto and TagDto

---
+++ Data/CatsDbContext.cs
@@ -0,0 +1,47 @@
+﻿using CatsAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatsAPI.Data
+{
+    public class CatsDbContext: DbContext
+    {
+        public CatsDbContext(DbContextOptions<CatsDbContext> dbContextoptions) : base(dbContextoptions)
+        {
+            
+        }
+
+        public DbSet<CatEntity> Cats { get; set; }
+        public DbSet<TagEntity> Tags { get; set; }
+
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CatEntity>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.CatId).IsRequired().HasMaxLength(100);
+                entity.HasIndex(c => c.CatId).IsUnique();
+                entity.Property(c => c.Height).IsRequired();
+                entity.Property(c => c.Width).IsRequired();
+                entity.Property(c => c.Image).IsRequired().HasMaxLength(1000);
+                entity.Property(c => c.Created).HasDefaultValueSql("GETDATE()");
+            });
+
+            modelBuilder.Entity<TagEntity>(entity =>
+            {
+                entity.HasKey(t => t.Id);
+                entity.Property(t => t.Name).IsRequired().HasMaxLength(100);
+                entity.Property(c => c.Created).HasDefaultValueSql("GETDATE()");
+            });
+
+
+            modelBuilder.Entity<CatEntity>(entity => 
+            {
+                entity.HasMany(c => c.Tags)
+                      .WithMany(t => t.Cats)
+                      .UsingEntity(j => j.ToTable("CatTags"));
+            });
+        }
+    }
+}
---
+++ Models/DTO/TagDto.cs
@@ -0,0 +1,11 @@
+﻿using CatsAPI.Models.Entities;
+
+namespace CatsAPI.Models.DTO
+{
+    public class TagDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
